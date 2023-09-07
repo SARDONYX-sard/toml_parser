39c14ccc7e7a28db40be76b597d78308be2b49e8
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-09-08 02:22:39 +0900

    feat!(parser): support string, integer

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,112 +1,256 @@
 import { TOMLParser } from './parser'; // Update the import path accordingly
 
-describe('takeUntil', () => {
-  test('should correctly take characters until the specified character is encountered', () => {
-    const tomlString = 'This is a test string.';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.takeUntil(' ');
-    expect(result).toEqual('this');
-  });
+describe('TOMLParser', () => {
+  describe('takeUntil', () => {
+    test('should correctly take characters until the specified character is encountered', () => {
+      const tomlString = 'This is a test string.';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.takeUntil(' ');
+      expect(result).toEqual('This');
+    });
 
-  test('should return the remaining string if the specified character is not found until the end', () => {
-    const tomlString = 'ThisIsAString';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.takeUntil(' ');
-    expect(result).toEqual('ThisIsAString');
-  });
-});
+    test('should return the remaining string if the specified character is not found until the end', () => {
+      const tomlString = 'ThisIsAString';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.takeUntil(' ');
+      expect(result).toEqual('ThisIsAString');
 
-describe('takeWhile', () => {
-  test('should correctly take characters while the predicate is true', () => {
-    const tomlString = '12345abc';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.takeWhile((char) => /\d/.test(char));
-    expect(result).toEqual('12345');
-  });
-
-  test('should return an empty string if the first character does not satisfy the predicate', () => {
-    const tomlString = 'abc12345';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.takeWhile((char) => /\d/.test(char));
-    expect(result).toEqual('');
-  });
-});
-
-describe('parseTableHeader', () => {
-  test('should correctly parse a simple table header', () => {
-    const tomlString = '[table1]';
-    const parser = new TOMLParser(tomlString);
-    parser.parseTableHeader();
-    const { result } = parser;
-    expect(result).toEqual({ table1: {} });
-  });
-
-  test('should correctly parse a nested table header', () => {
-    const tomlString = '[table1.sub_table]\nkey="value"';
-    const parser = new TOMLParser(tomlString);
-    parser.parseTableHeader();
-    const { result } = parser;
-    expect(result).toEqual({ table1: { sub_table: {} } });
-  });
-
-  test('[Err]should throw an error for an invalid table header', () => {
-    const invalidStr = ']'; // Yes, Error ocurred because unclosed pair.
-    const tomlString = `[table1.invalid]${invalidStr}`;
-    const parser = new TOMLParser(tomlString);
-    expect(() => parser.parseTableHeader()).toThrow(
-      `A newline or EOF is expected after the table header. But got ${invalidStr}`,
-    );
-  });
-});
-
-describe('parseTableContents', () => {
-  test('should correctly parse table contents with key-value pairs', () => {
-    const tomlString = 'key1 = "value1"\nkey2 = "value2"';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.parseTableContents();
-    expect(result).toEqual({ key1: 'value1', key2: 'value2' });
-  });
-
-  test('should correctly parse nested key-value pairs', () => {
-    const tomlString = 'outer.inner1 = "value1"\nouter.inner2 = "value2"';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.parseTableContents();
-    expect(result).toEqual({
-      outer: {
-        inner1: 'value1',
-        inner2: 'value2',
-      },
+      expect(parser.index).toEqual(tomlString.length);
     });
   });
 
-  test('should correctly parse an empty table', () => {
-    const tomlString = '';
-    const parser = new TOMLParser(tomlString);
-    const result = parser.parseTableContents();
-    expect(result).toEqual({});
+  describe('takeWhile', () => {
+    test('should correctly take characters while the predicate is true', () => {
+      const tomlString = '12345abc';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.takeWhile((char) => /\d/.test(char));
+      expect(result).toEqual('12345');
+
+      const { index } = parser;
+      expect(index).toEqual(5);
+    });
+
+    test('should return an empty string if the first character does not satisfy the predicate', () => {
+      const tomlString = 'abc12345';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.takeWhile((char) => /\d/.test(char));
+      expect(result).toEqual('');
+    });
   });
 
-  test('should correctly parse table contents with nested key-value pairs and arrays', () => {
-    const tomlString = `
-        tauri = {
-          version = "1.3.0",
-          features = [
-            "api-all",
-            "icon-ico",
-            "icon-png",
-            "system-tray"
-          ]
-        }`;
+  describe('parseTableHeader', () => {
+    test('should correctly parse a simple table header', () => {
+      const tomlString = '[table1]';
+      const parser = new TOMLParser(tomlString);
+      parser.parseTableHeader();
+      const { result } = parser;
+      expect(result).toEqual({ table1: {} });
+    });
 
-    const parser = new TOMLParser(tomlString);
-    const result = parser.parseTableContents();
-    console.debug('%o', result);
+    test('should correctly parse a nested table header', () => {
+      const tomlString = '[table1.sub_table]\nkey="value"';
+      const parser = new TOMLParser(tomlString);
+      parser.parseTableHeader();
+      const { result } = parser;
+      expect(result).toEqual({ table1: { sub_table: {} } });
+    });
 
-    expect(result).toEqual({
-      tauri: {
-        version: '1.3.0',
-        features: ['api-all', 'icon-ico', 'icon-png', 'system-tray'],
-      },
+    test('[Err]should throw an error for an invalid table header', () => {
+      const invalidStr = ']'; // Yes, Error ocurred because unclosed pair.
+      const tomlString = `[table1.invalid]${invalidStr}`;
+      const parser = new TOMLParser(tomlString);
+      expect(() => parser.parseTableHeader()).toThrow(
+        `A newline or EOF is expected after the table header. But got ${invalidStr}`,
+      );
+    });
+  });
+
+  describe('parseTableContents', () => {
+    test('should correctly parse table contents with key-value pairs', () => {
+      const tomlString = 'key1 = "value1"\nkey2 = "value2"';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseTableContents();
+      expect(result).toEqual({ key1: 'value1', key2: 'value2' });
+    });
+
+    test('should correctly parse nested key-value pairs', () => {
+      const tomlString = 'outer.inner1 = "value1",outer.inner2 = "value2"';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseTableContents(',');
+      expect(result).toEqual({
+        outer: {
+          inner1: 'value1',
+          inner2: 'value2',
+        },
+      });
+    });
+
+    test('should correctly parse an empty table', () => {
+      const tomlString = '';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseTableContents();
+      expect(result).toEqual({});
+    });
+
+    //! Array parse not yet.
+    // test('should correctly parse table contents with nested key-value pairs and arrays', () => {
+    //   const tomlString = `
+    //     tauri = {
+    //       version = "1.3.0",
+    //       features = [
+    //         "api-all",
+    //         "icon-ico",
+    //         "icon-png",
+    //         "system-tray"
+    //       ]
+    //     }`;
+
+    //   const parser = new TOMLParser(tomlString);
+    //   const result = parser.parseTableContents(',');
+    //   console.debug('%o', result);
+
+    //   expect(result).toEqual({
+    //     tauri: {
+    //       version: '1.3.0',
+    //       features: ['api-all', 'icon-ico', 'icon-png', 'system-tray'],
+    //     },
+    //   });
+    // });
+  });
+
+  //! Unimplemented Array parser yet.
+  // describe('parseArrayValue', () => {
+  //   test('should correctly parse a simple array', () => {
+  //     const tomlString = '[1, 2, 3, 4, 5]';
+  //     const parser = new TOMLParser(tomlString);
+  //     const result = parser.parseArrayValue();
+  //     expect(result).toEqual(['1', '2', '3', '4', '5']);
+  //   });
+
+  //   test('should correctly parse an array with mixed data types', () => {
+  //     const tomlString = '["a", 1, true, 2.5, null]';
+  //     const parser = new TOMLParser(tomlString);
+  //     const result = parser.parseArrayValue();
+  //     expect(result).toEqual(['"a"', ' 1', ' true', ' 2.5', ' null']);
+  //   });
+
+  //   test('should correctly parse an empty array', () => {
+  //     const tomlString = '[]';
+  //     const parser = new TOMLParser(tomlString);
+  //     const result = parser.parseArrayValue();
+  //     expect(result).toEqual([]);
+  //   });
+  // });
+
+  describe('parseStringValue', () => {
+    test('should correctly parse a double-quoted string', () => {
+      const tomlString = '"This is a double-quoted string"';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseStringValue('"');
+      expect(result).toEqual('This is a double-quoted string');
+    });
+
+    test('should correctly parse a single-quoted string', () => {
+      const tomlString = "'This is a single-quoted string'";
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseStringValue("'");
+      expect(result).toEqual('This is a single-quoted string');
+    });
+
+    test('should handle escaped quotes within a string', () => {
+      const tomlString = '"This is a \\"quoted\\" string"';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseStringValue('"');
+      expect(result).toEqual('This is a \\"quoted\\" string');
+    });
+
+    test('should handle empty strings', () => {
+      const tomlString = '""';
+      const parser = new TOMLParser(tomlString);
+      const result = parser.parseStringValue('"');
+      expect(result).toEqual('');
+    });
+  });
+
+  describe('parseNumberValue', () => {
+    test('should parse positive integer without numeric separators', () => {
+      const parser = new TOMLParser('42');
+      expect(parser.parseNumberValue()).toEqual('42');
+    });
+
+    test('should parse positive integer with numeric separators', () => {
+      const parser = new TOMLParser('1_000_000');
+      expect(parser.parseNumberValue()).toEqual('1_000_000');
+    });
+
+    test('should parse negative integer without numeric separators', () => {
+      const parser = new TOMLParser('-123');
+      expect(parser.parseNumberValue()).toEqual('-123');
+    });
+
+    test('should parse negative integer with numeric separators', () => {
+      const parser = new TOMLParser('-1_234_567');
+      expect(parser.parseNumberValue()).toEqual('-1_234_567');
+    });
+
+    test('should parse positive float without numeric separators', () => {
+      const parser = new TOMLParser('3.14159');
+      expect(parser.parseNumberValue()).toEqual('3.14159');
+    });
+
+    test('should parse positive float with numeric separators', () => {
+      const parser = new TOMLParser('3.1_4159e-10');
+      expect(parser.parseNumberValue()).toEqual('3.1_4159e-10');
+    });
+
+    test('should parse negative float without numeric separators', () => {
+      const parser = new TOMLParser('-2.71828');
+      expect(parser.parseNumberValue()).toEqual('-2.71828');
+    });
+
+    test('should parse negative float with numeric separators', () => {
+      const parser = new TOMLParser('-2.71_828e+10');
+      expect(parser.parseNumberValue()).toEqual('-2.71_828e+10');
+    });
+
+    test('should parse hexadecimal number without numeric separators', () => {
+      const parser = new TOMLParser('0x1A3');
+      expect(parser.parseNumberValue()).toEqual('0x1A3');
+    });
+
+    test('should parse hexadecimal number with numeric separators', () => {
+      const parser = new TOMLParser('0x1_A3');
+      expect(parser.parseNumberValue()).toEqual('0x1_A3');
+    });
+
+    test('should throw an error for invalid hexadecimal characters', () => {
+      const parser = new TOMLParser('0x1G3');
+      expect(() => parser.parseNumberValue()).toThrow('Invalid characters in hexadecimal number.');
+    });
+
+    test('should parse special float value "inf"', () => {
+      const parser = new TOMLParser('inf');
+      expect(parser.parseNumberValue()).toEqual('inf');
+    });
+
+    test('should parse special float value "+inf"', () => {
+      const parser = new TOMLParser('+inf');
+      expect(parser.parseNumberValue()).toEqual('+inf');
+    });
+
+    test('should parse special float value "-inf"', () => {
+      const parser = new TOMLParser('-inf');
+      expect(parser.parseNumberValue()).toEqual('-inf');
+    });
+
+    test('should parse special float value "nan"', () => {
+      const parser = new TOMLParser('nan');
+      expect(parser.parseNumberValue()).toEqual('nan');
+    });
+
+    test('should throw an error for unexpected character', () => {
+      const parser = new TOMLParser('3.14x');
+      expect(() => parser.parseNumberValue()).toThrow('Unknown tailing x. Do you mean 3.14?');
     });
   });
 });
--- src/parser.ts
+++ src/parser.ts
@@ -63,7 +63,7 @@ parsed toml: ${JSON.stringify(this.result)}`);
   parseTableHeader() {
     this.advance(); // Skip `[`
     const tableHeader = this.takeUntil(']');
-    this.advance(); // Skip `]`
+    this.advance(); // current index point to `]`, So we Skip `]`
 
     // remain check for syntax errors
     const remain = this.takeUntil('\n');
@@ -78,6 +78,7 @@ parsed toml: ${JSON.stringify(this.result)}`);
 
   /**
    * Parse table contents
+   * @param delim - default: `\n`(e.g. `\n`, `,` etc.)
    *
    * - from
    * ```toml
@@ -103,18 +104,18 @@ parsed toml: ${JSON.stringify(this.result)}`);
    * }
    * ```
    */
-  parseTableContents(): TomlJson {
+  parseTableContents(delim = '\n'): TomlJson {
     // take until next table header symbol.
     let table = {};
     this.takeWhile((char) => {
+      this.takeWhile((char) => char === delim);
       // parse key value(e.g. key= value)
       const nestedKeys = splitKeyStr(this.takeUntil('=').trim());
-      this.advance(); // Skip `=`
+      this.advance(); // Skip `='
       const obj = arrayToNestObj(nestedKeys);
       const value = this.parseValue();
       setValueToObject(obj, nestedKeys, value);
       table = deepMerge(table, obj);
-      this.takeWhile((char) => char === '\n' || char === ',');
       return char !== '[';
     });
 
@@ -124,7 +125,8 @@ parsed toml: ${JSON.stringify(this.result)}`);
   parseValue(): TomlValue {
     while (this.index < this.text.length) {
       const char = this.text[this.index];
-      if (char === ' ' || char === '\t') {
+      // - `,`: array delimiter.
+      if (char === ' ' || char === '\t' || char === ',') {
         this.advance();
       } else if (char === '{') {
         return this.parseTableValue();
@@ -136,7 +138,7 @@ parsed toml: ${JSON.stringify(this.result)}`);
         return this.parseStringValue("'");
       } else if (char === 't' || char === 'f') {
         return this.parseBoolValue();
-      } else if (!Number.isNaN(char) || char === '-') {
+      } else if (!isNaN(Number(char)) || char === '-') {
         return this.parseNumberValue();
       } else {
         throw new Error(`Unexpected character: ${char}`);
@@ -149,11 +151,10 @@ parsed toml: ${JSON.stringify(this.result)}`);
    *
    * @private
    * @return {*}
-   * @memberof TOMLParser
    */
   parseTableValue() {
     this.advance(); // Skip `{`
-    const res = this.parseTableContents();
+    const res = this.parseTableContents(',');
     this.advance(); // Skip `}`
     return res;
   }
@@ -175,30 +176,34 @@ parsed toml: ${JSON.stringify(this.result)}`);
   }
 
   parseArrayValue(): Array<TomlValue> {
-    const start = this.index;
-    let depth = 1;
-
-    while (depth > 0) {
-      this.advance();
-      const char = this.text[this.index];
-
-      if (char === '[') {
-        depth++;
-      } else if (char === ']') {
-        depth--;
-      }
-    }
-    return this.text.slice(start, this.index + 1).split(',');
+    const res: Array<TomlValue> = [];
+    this.advance(); // Skip opening `[` bracket
+    res.push(this.parseValue());
+    this.takeUntil(']'); // remove whitespace
+    this.advance(); // Skip opening `]` bracket
+    return res;
   }
 
   parseStringValue(pairChar: string): string {
     this.advance(); // Skip opening quote
-    return this.takeUntil(pairChar); // index now points to the closed quote string.
+    let isEscaped = false; //Forced read flag to deal with parsing of "\", \'.
+    const res = this.takeWhile((char) => {
+      if (char === '\\' && this.text[this.index + 1] === pairChar) {
+        isEscaped = true;
+        return true;
+      } else if (isEscaped) {
+        isEscaped = false;
+        return true;
+      } else {
+        return char !== pairChar;
+      }
+    }); // index now points to the closed quote string.
+    this.advance(); // Skip closing quote
+    return res;
   }
 
   parseBoolValue(): boolean {
-    const boolStr = this.takeWhile((char) => /[a-zA-Z]/.test(char));
-    return boolStr === 'true';
+    return this.takeUntil('\n') === 'true';
   }
 
   /**
@@ -206,41 +211,69 @@ parsed toml: ${JSON.stringify(this.result)}`);
    *
    * @returns The parsed number string(e.g. "0xFFF", "0o00", "0b1011")
    */
-  parseNumberValue(): string {
-    const isHex = () => this.text[this.index] === '0' && this.text[this.index + 1] === 'x';
-    const isOctal = () => this.text[this.index] === '0' && this.text[this.index + 1] === 'o';
-    const isBinary = () => this.text[this.index] === '0' && this.text[this.index + 1] === 'b';
+  parseNumberValue(): TomlValue {
+    const mayNum = this.takeUntil('\n').trim();
 
-    if (isHex()) {
-      this.advance(); // Skip '0'
-      this.advance(); // Skip 'x'
-      const hexStr = this.takeWhile((char) => /[0-9a-fA-F]/.test(char));
-      return `0x${hexStr}`;
-    } else if (isOctal()) {
-      this.advance(); // Skip '0'
-      this.advance(); // Skip 'o'
-      const octalStr = this.takeWhile((char) => /[0-7]/.test(char));
-      return `0o${octalStr}`;
-    } else if (isBinary()) {
-      this.advance(); // Skip '0'
-      this.advance(); // Skip 'b'
-      const binaryStr = this.takeWhile((char) => /[0-1]/.test(char));
-      return `0b${binaryStr}`;
+    // Check for a numeric separator at the end and disallow it
+    if (mayNum.endsWith('_')) {
+      throw new Error('Numeric separator (_) not allowed at the end of a number value.');
+    }
+
+    if (mayNum.startsWith('0x')) {
+      // Hexadecimal number
+      const hexStr = mayNum.slice(2);
+      if (/^[\da-fA-F]+(?:_[\da-fA-F]+)*\b/.test(hexStr)) {
+        return `0x${hexStr}`;
+      }
+      throw new Error('Invalid characters in hexadecimal number.');
+    } else if (mayNum.startsWith('0o')) {
+      // Octal number
+      const octalStr = mayNum.slice(2);
+      if (/^[0-7]+(?:_[0-7]+)*\b/.test(octalStr)) {
+        return `0o${octalStr}`;
+      }
+      throw new Error('Invalid characters in octal number.');
+    } else if (mayNum.startsWith('0b')) {
+      // Binary number
+      const binaryStr = mayNum.slice(2);
+      if (/^[01]+(?:_[01]+)*\b/.test(binaryStr)) {
+        return `0b${binaryStr}`;
+      }
+      throw new Error('Invalid characters in binary number.');
+    } else if (/^[-+]?\d+(?:_|\d+)*(?:\.\d+(?:[eE]|[-+]|_|\d+)*)?\b/.test(mayNum)) {
+      // This RegExp borrowed from PrismJS
+      // MIT LICENSE
+      // Copyright (c) 2012 Lea Verou
+      // See: https://github.com/PrismJS/prism/pull/1488/files#diff-f262e20be4431bcbc6bb70f9fa7a00ad070d706ed9c4eb7310ee049890a49474R39
+      const [, num, remain] = /(^[-+]?\d+(?:_|\d+)*(?:\.\d+(?:[eE]|[-+]|_|\d+)*)?)(.*)/.exec(mayNum);
+      if (remain) {
+        throw new Error(`Unknown tailing ${remain}. Do you mean ${num}?`);
+      }
+      return mayNum; // decimal & float
+    } else if (mayNum === 'inf' || mayNum === '+inf' || mayNum === '-inf' || mayNum === 'nan') {
+      // Special float values
+      return mayNum;
     } else {
-      return this.takeWhile((char) => /[0-9.eE+-]/.test(char));
+      throw new Error(`Invalid number format: ${mayNum}`);
     }
   }
 
   /**
-   *
-   * @param {string} char
-   * @return {string} String
-   * @memberof TOMLParser
+   * - Reads a string up to the argument `char`
+   * - After this function, `this.index` points to the position **immediately before** the `char` argument.
+   * @param char
+   * @returns String up to argument `char`
    */
   takeUntil(char: string): string {
     return this.takeWhile((current_char) => current_char !== char);
   }
 
+  /**
+   * - Get the characters while predicate is true
+   * - After this function ends, this.index points to the next character.(if predicate conditional is true)
+   * @param predicate
+   * @returns the characters while predicate is true.
+   */
   takeWhile(predicate: (char: string) => boolean): string {
     let value = '';
     while (this.index < this.text.length && predicate(this.text[this.index])) {
